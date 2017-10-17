d4683e037563579d21e5929f6383d845f8be7db0
BenHNet <ben@htechinc.com>
2017-10-17 09:22:50 -0600

    Got the sample clients authenticating using my htech identity server.

--- CoreAPI/CoreAPI/Controllers/TourOfHeroesController.cs
+++ CoreAPI/CoreAPI/Controllers/TourOfHeroesController.cs
@@ -9,33 +9,43 @@ namespace CoreAPI.Controllers
     [Route("api/[controller]")]
     public class TourOfHeroesController : Controller
     {
-        // GET api/values
+        // GET api/tourofheroes
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "{ id: 0, name: 'Zero' }",
+                "{ id: 11, name: 'Mr. Nice' }",
+                "{ id: 12, name: 'Narco' }",
+                "{ id: 13, name: 'Bombasto' }",
+                "{ id: 14, name: 'Celeritas' }",
+                "{ id: 15, name: 'Magneta' }",
+                "{ id: 16, name: 'RubberMan' }",
+                "{ id: 17, name: 'Dynama' }",
+                "{ id: 18, name: 'Dr IQ' }",
+                "{ id: 19, name: 'Magma' }",
+                "{ id: 20, name: 'Tornado' }" };
         }
 
-        // GET api/values/5
+        // GET api/tourofheroes/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/tourofheroes
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/tourofheroes/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/tourofheroes/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
---
+++ SampleApps/src/ResourceOwnerClient/Program.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ResourceOwnerClient
+{
+    public class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            // discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000/");
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "sample_username_client", "sample_9TKTb5Th4LpP");
+            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("alice", "alice", "sample_api");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
